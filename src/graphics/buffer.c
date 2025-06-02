15b314450a53929a2678344d470d8e5d8e969381
rayenxgamer <rayenxgamer@gmail.com>
2025-06-02 14:50:27 +0000

    added and finished buffer translation unit and header

--- src/graphics/buffer.c
+++ src/graphics/buffer.c
@@ -1,8 +1,12 @@
 #include <graphics/buffer.h>
 
-
-uint32_t vao_create(uint32_t *vao){
+uint32_t vao_create(){
+  uint32_t *vao;
   glGenVertexArrays(1, vao);
+  if(*vao == GL_INVALID_VALUE){
+    printf("invalid vao generated!\n");
+    return 0;
+  }
   return *vao;
 };
 
@@ -10,15 +14,78 @@ void vao_bind(uint32_t *vao){
   glBindVertexArray(*vao);
 };
 
-uint32_t vbo_create(uint32_t *vbo){
+void vao_attrib(uint32_t vao, uint32_t vbo, GLuint index, GLint size
+,GLenum type, GLboolean normalized, GLsizei stride,size_t offset){
+
+  if (vao < 0)
+    printf("invalid vao!\n");
+  vao_bind(&vao);
+  if (vbo < 0)
+    printf("invalid vbo!\n");
+  vbo_bind(&vbo);
+
+  switch (type) {
+    case GL_FLOAT:
+      glVertexAttribPointer(index, size, type, normalized, stride, (GLvoid*)offset);
+    case GL_INT:
+      glVertexAttribIPointer(index, size, normalized, stride, (GLvoid*)offset);
+    case GL_BYTE:
+    case GL_UNSIGNED_BYTE:
+    case GL_SHORT:
+    case GL_UNSIGNED_SHORT:
+    case GL_UNSIGNED_INT:
+    case GL_HALF_FLOAT:
+    case GL_DOUBLE:
+    case GL_INT_2_10_10_10_REV:
+      glVertexAttribIPointer(index, size, normalized, stride, (GLvoid*)offset);
+    case GL_UNSIGNED_INT_2_10_10_10_REV:
+    default:
+      printf("enter a proper type for the vao!\n");
+    break;
+  }
+  glEnableVertexAttribArray(index);
+};
+
+uint32_t vbo_create(){
+  uint32_t *vbo;
   glGenBuffers(1, vbo);
+  if (*vbo == GL_INVALID_VALUE){
+    printf("invalid vbo generated!\n");
+    return 0;
+  }
   return *vbo;
 };
 
 void vbo_bind(uint32_t *vbo){
   glBindBuffer(GL_ARRAY_BUFFER, *vbo);
+  if (*vbo == GL_INVALID_VALUE){
+    printf("you're binding a vertex buffer that wasnt previously made! make sure to bind a proper buffer\n");
+    return;
+  }
 };
 
+void vbo_buffer(size_t size, void* data, GLenum usage){
+  glBufferData(GL_ARRAY_BUFFER, size, data, usage);
+}
 
+uint32_t ibo_create(){
+  uint32_t *ibo;
+  glGenBuffers(1, ibo);
+  if (*ibo == GL_INVALID_VALUE){
+    printf("invalid vbo generated!\n");
+    return 0;
+  }
+  return *ibo;
+}
 
+void ibo_bind(uint32_t *ibo){
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *ibo);
+  if (*ibo == GL_INVALID_VALUE){
+    printf("you're binding a vertex buffer that wasnt previously made! make sure to bind a proper buffer\n");
+    return;
+  }
+};
 
+void ibo_buffer(size_t size, void *data, GLenum usage){
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, data, usage);
+}
--- src/graphics/buffer.h
+++ src/graphics/buffer.h
@@ -1,13 +1,19 @@
 #pragma once
 
 #include <glad/gl.h>
+#include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 
-uint32_t vao_create(uint32_t* vao);
+uint32_t vao_create();
 void vao_bind(uint32_t* vao);
+void vao_attrib(uint32_t vao, uint32_t vbo, GLuint index, GLint size
+,GLenum type, GLboolean normalized, GLsizei stride,size_t offset);
 
-uint32_t vbo_create(uint32_t* vbo);
+uint32_t vbo_create();
 void vbo_bind(uint32_t* vbo);
+void vbo_buffer(size_t size, void* data, GLenum usage);
 
-uint32_t inb_create(uint32_t* inb);
-void inb_bind(uint32_t* inb);
+uint32_t ibo_create();
+void ibo_bind(uint32_t* inb);
+void ibo_buffer(size_t size, void* data, GLenum usage);
