04d0d4b219742fce41a1cfef3d609fea1d212052
rayenxgamer <rayenxgamer@gmail.com>
2025-06-11 02:15:31 +0000

    overhawled the entire window system, aswell as added a deltatime variable and a tick system alongside it!

--- src/game/game.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <glad/gl.h>
-#include <GLFW/glfw3.h>
-#include <stdio.h>
-
-int game_run(GLFWwindow* window);
-void game_update(GLFWwindow* window);
-void game_shutdown(GLFWwindow* window);
