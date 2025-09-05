26bd0e09d5e7867b725e219e30b6c1b16796fead
rayenxgamer <rayenxgamer@gmail.com>
2025-09-05 03:50:49 +0100

    updated renmath.h to have consistent header include styles

--- src/math/renmath.h
+++ src/math/renmath.h
@@ -2,12 +2,12 @@
 
 #include "math/common.h"
 #include "math/projection.h"
-#include <math/aabb.h>
-#include <math/vec2.h>
-#include <math/mat3.h>
-#include <math/vec3.h>
-#include <math/vec4.h>
-#include <math/mat4.h>
+#include "math/aabb.h"
+#include "math/vec2.h"
+#include "math/mat3.h"
+#include "math/vec3.h"
+#include "math/vec4.h"
+#include "math/mat4.h"
 
 // THIS IS TEMPORARY UNTILL I GET EVERYTHING READY FOR RENGINE.h
 // sorry for the yelling :D
